1191239c79a0d6d9692d3f80fd3fec8c690f20da
habul17 <abulhassanca.dev@gmail.com>
2026-07-17 12:57:07 +0530

    Add Authentication

--- index.js
+++ index.js
@@ -1,10 +1,80 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
 
+// In-Memory Database
+
+const USERS = [];
+const ORGANIZATIONS = [];
+const BOARDS = [];
+const ISSUES = [];
+
+
+// POST ENDPOINTS
+
+
+// POST - SIGNUP
+
+app.post("/signup", (req, res) => {
+
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    const userExist = USERS.find(user => user.userName === userName);
+
+    if (userExist) {
+
+        res.status(400).json({
+            message : "User already exists"
+        })
+        return;
+
+    }
+
+    USERS.push({
+        userName,
+        password
+    })
+
+    res.status(201).json({
+        message : "User created successfully"
+    })
+
+})
+
+// POST - SIGNIN
+
+app.post("/signin", (req, res) => {
+
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    const userExist = USERS.find(user => user.userName === userName && user.password === password);
+
+    if (!userExist) {
+
+        res.status(403).json({
+            message : "Invalid Credentials"
+        })
+        return;
+
+    }
+
+    const token = jwt.sign({userName}, process.env.JWT_SECRET);
+
+    res.json({
+        message : "Sign Up Successfull",
+        token
+    })
+
+
+
+})
 
 
 app.listen(3000);
